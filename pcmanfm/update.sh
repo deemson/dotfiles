d6b7aac47864cbbfcd2df9f262c75e38188822b8
Kurkin <deemson.k@gmail.com>
2021-05-21 18:15:17 +0300

    added clarifying comment to pcmanfm update script

--- pcmanfm/update.sh
+++ pcmanfm/update.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
+# Set window width & height to certain hardcoded values
+# It is required because PCManFM keeps changing those on every app exit
+# and config keeps changing in git repo because of that
 cat $HOME/.config/pcmanfm/default/pcmanfm.conf | sed 's/win_width=.*/win_width=640/; s/win_height=.*/win_heigh=480/' >pcmanfm.conf
