2246294c49e865eefa2d461a4bb4eaecdc6b8593
Kurkin <deemson.k@gmail.com>
2021-05-31 12:09:47 +0300

    i3blocks: updated battery block to serve multiple batteries

--- i3blocks/blocks/battery.sh
+++ i3blocks/blocks/battery.sh
@@ -9,26 +9,33 @@ bat_icon_half=
 bat_icon_quarter=
 bat_icon_empty=
 ac_icon=
+batstatus=""
 
-battery=$(cat /sys/class/power_supply/BAT0/capacity)
-if (($battery == 100)); then
-    battery=..
-    bicon=$bat_icon_full
-elif (($battery > 90)); then
-    bicon=$bat_icon_full
-elif (($battery > 60)); then
-    bicon=$bat_icon_three_quarters
-elif (($battery > 40)); then
-    bicon=$bat_icon_half
-elif (($battery > 10)); then
-    bicon=$bat_icon_quarter
-else
-    bicon=$bat_icon_empty
-fi
+for entry in /sys/class/power_supply/*; do
+    if [[ $(basename $entry) == "BAT"* ]]; then
+        battery=$(cat $entry/capacity)
+        if (($battery == 100)); then
+            battery=..
+            bicon=$bat_icon_full
+        elif (($battery > 90)); then
+            bicon=$bat_icon_full
+        elif (($battery > 60)); then
+            bicon=$bat_icon_three_quarters
+        elif (($battery > 40)); then
+            bicon=$bat_icon_half
+        elif (($battery > 10)); then
+            bicon=$bat_icon_quarter
+        else
+            bicon=$bat_icon_empty
+        fi
+        battery=$(printf %02d $battery)
+        batstatus+=" $bicon $battery"
+    fi
+done
 ac=$(cat /sys/class/power_supply/AC/online)
 if (($ac == 1)); then
     ac_color=$col_fg
 else
     ac_color=$col_bg1
 fi
-printf "$lsep $bicon $battery <span fgcolor=\"$ac_color\">$ac_icon</span> $rsep\\n"
+printf "$lsep$batstatus <span fgcolor=\"$ac_color\">$ac_icon</span> $rsep\\n"
--- i3blocks/install.sh
+++ i3blocks/install.sh
@@ -3,6 +3,6 @@
 set -x
 mkdir -p $HOME/.config/i3blocks/blocks
 cp config $HOME/.config/i3blocks/config
-cp colors.sh $HOME/.config/i3blocks/config.sh
+cp colors.sh $HOME/.config/i3blocks/colors.sh
 cp common.sh $HOME/.config/i3blocks/common.sh
 cp blocks/* $HOME/.config/i3blocks/blocks/
