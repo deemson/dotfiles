fd9f5ccf9a0d301541de737ac594df4a4f4a9962
Kurkin <deemson.k@gmail.com>
2021-08-01 14:18:34 +0300

    added config for VS code

---
+++ vs-code/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+set -x
+mkdir -p $HOME/.config/Code/User
+cp settings.json $HOME/.config/Code/User/settings.json
---
+++ vs-code/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+set -x
+cp -r $HOME/.config/Code/User/settings.json ./settings.json
