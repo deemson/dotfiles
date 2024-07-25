4bd69c12a8f35436568e4109bc0305b11ea775f8
deemson <deemson.k@gmail.com>
2024-07-25 16:32:39 +0300

    starship: config and save command

--- cli/src/main.js
+++ cli/src/main.js
@@ -1,5 +1,7 @@
 import { program } from 'commander'
 import { neovimCommands } from './cmd/neovim.js'
+import { starshipCommands } from './cmd/sharship.js'
 
 neovimCommands()
+starshipCommands()
 program.parse()
