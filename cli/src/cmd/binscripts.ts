292b0d8e2688b6b57b32fc268677013c622f8dee
deemson <deemson.k@gmail.com>
2024-12-21 22:29:16 +0100

    cli: binscripts commands; binscripts: initial

---
+++ cli/src/cmd/binscripts.ts
@@ -0,0 +1,39 @@
+import { program } from 'commander'
+import { logger } from '../lib/logging.ts'
+import fs from 'node:fs/promises'
+import { copyDirContents } from '../lib/fsutils.ts'
+import path from 'node:path'
+import { dotfilesDir, homeDir } from '../lib/paths.ts'
+
+export const binscriptsCommands = () => {
+  const repoDir = path.join(dotfilesDir, 'binscripts')
+  const systemDir = path.join(homeDir, '.bin')
+  const filePaths = ['dotfiles', 'dotfiles-save', 'dotfiles-load'].map(fileName => ({
+    repo: path.join(repoDir, fileName),
+    system: path.join(systemDir, fileName)
+  }))
+
+  const binScripts = program.command('binscripts')
+
+  binScripts.command('save')
+    .action(async () => {
+      logger.info({ dir: repoDir }, 'cleaning')
+      await fs.rm(repoDir, { recursive: true, force: true })
+      await fs.mkdir(repoDir, { recursive: true })
+      for (const filePath of filePaths) {
+        logger.info({ from: filePath.system, to: filePath.repo }, 'saving')
+        await fs.copyFile(filePath.system, filePath.repo)
+      }
+      logger.info({ from: systemDir, to: repoDir }, 'done')
+    })
+
+  binScripts.command('load')
+    .action(async () => {
+      await fs.mkdir(systemDir, { recursive: true })
+      for (const filePath of filePaths) {
+        logger.info({ from: filePath.repo, to: filePath.system }, 'loading')
+        await fs.copyFile(filePath.repo, filePath.system)
+      }
+      logger.info({ from: repoDir, to: systemDir }, 'done')
+    })
+}
--- cli/src/main.ts
+++ cli/src/main.ts
@@ -1,9 +1,11 @@
 import { program } from 'commander'
+import { binscriptsCommands } from './cmd/binscripts.ts'
 import { neovimCommands } from './cmd/neovim.ts'
 import { starshipCommands } from './cmd/starship.ts'
 import { zshCommands } from './cmd/zsh.ts'
 import { weztermCommands } from './cmd/wezterm.ts'
 
+binscriptsCommands()
 neovimCommands()
 starshipCommands()
 zshCommands()
