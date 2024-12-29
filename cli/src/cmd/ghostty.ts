d54dbf6e72ad37728adcafba21e56a174737d6c4
deemson <deemson.k@gmail.com>
2024-12-29 10:41:31 +0100

    ghostty: CLI support & config

---
+++ cli/src/cmd/ghostty.ts
@@ -0,0 +1,30 @@
+import { program } from 'commander'
+import { dotfilesDir, homeDir } from '../lib/paths.ts'
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { logger } from '../lib/logging.ts'
+
+export const ghosttyCommands = () => {
+  const repoDir = path.join(dotfilesDir, 'ghostty')
+  const repoConfig = path.join(repoDir, 'config')
+  const systemDir = path.join(homeDir, '.config', 'ghostty')
+  const systemConfig = path.join(systemDir, 'config')
+
+  const ghostty = program.command('ghostty')
+
+  ghostty.command('save')
+    .action(async () => {
+      logger.info({ from: systemConfig, to: repoConfig }, 'saving')
+      await fs.mkdir(repoDir, { recursive: true })
+      await fs.copyFile(systemConfig, repoConfig)
+      logger.info({ from: systemConfig, to: repoConfig }, 'done')
+    })
+
+  ghostty.command('load')
+    .action(async () => {
+      logger.info({ from: repoConfig, to: systemConfig }, 'loading')
+      await fs.mkdir(systemDir, { recursive: true })
+      await fs.copyFile(repoConfig, systemConfig)
+      logger.info({ from: repoConfig, to: systemConfig }, 'done')
+    })
+}
--- cli/src/main.ts
+++ cli/src/main.ts
@@ -4,10 +4,12 @@ import { neovimCommands } from './cmd/neovim.ts'
 import { starshipCommands } from './cmd/starship.ts'
 import { zshCommands } from './cmd/zsh.ts'
 import { weztermCommands } from './cmd/wezterm.ts'
+import { ghosttyCommands } from './cmd/ghostty.ts'
 
 binscriptsCommands()
 neovimCommands()
 starshipCommands()
 zshCommands()
 weztermCommands()
+ghosttyCommands()
 program.parse()
