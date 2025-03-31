ee4e07b9ba0218b1c49e597d60e47650fcac2753
deemson <deemson.k@gmail.com>
2025-03-31 08:48:24 +0200

    neovim: blink cmp keymap

--- dotfiles/neovim/lua/plugins/blink-cmp.lua
+++ dotfiles/neovim/lua/plugins/blink-cmp.lua
@@ -3,5 +3,20 @@ return {
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
-  opts = { fuzzy = { implementation = 'rust' } }
+  opts = {
+    fuzzy = { implementation = 'rust' },
+    keymap = {
+      preset = 'none',
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<S-Tab>'] = { 'hide' },
+      ['<Tab>'] = { 'select_and_accept' },
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' }
+    }
+  }
 }
