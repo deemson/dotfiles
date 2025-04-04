6cc286361acb5d446f1ee78af3781113c01e2001
deemson <deemson.k@gmail.com>
2025-04-04 22:44:42 +0200

    neovim: configure winbar for oil to show current directory

--- dotfiles/neovim/lua/config/plugins/oil.lua
+++ dotfiles/neovim/lua/config/plugins/oil.lua
@@ -1,8 +1,9 @@
 require('oil').setup({
   watch_for_changes = true,
   win_options = {
-    signcolumn = "yes:2",
-    winbar = "hello"
+    signcolumn = 'yes:2',
+    -- display current directory at the top and replace part equal to $HOME with ~
+    winbar = '%#@attribute.builtin#%{substitute(v:lua.require("oil").get_current_dir(), "^" . $HOME, "~", "")}'
   }
 })
 
