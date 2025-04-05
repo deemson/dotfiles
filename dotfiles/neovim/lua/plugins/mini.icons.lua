e8f4d278400a8dd3d5f5a77e7a19bde75ab498c0
deemson <deemson.k@gmail.com>
2025-04-05 17:08:05 +0200

    neovim: migrated back to nvim-web-devicons

--- dotfiles/neovim/lua/plugins/mini.icons.lua
+++
@@ -1,2 +0,0 @@
--- mini.icons are icons for various stuff
-return { 'echasnovski/mini.icons', version = '*' }
---
+++ dotfiles/neovim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,2 @@
+-- nvim-web-devicons are icons for various stuff
+return { 'nvim-tree/nvim-web-devicons', version = '*' }
