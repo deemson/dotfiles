9cec1d9b04971104435f52d9dc4d21a8bd0bc75b
deemson <deemson.k@gmail.com>
2025-05-06 11:42:57 +0200

    neovim: nuke neotest & DAP

--- dotfiles/neovim/lua/plugins/nvim-dap-ui.lua
+++
@@ -1,5 +0,0 @@
--- nvim-dap-ui is a UI for nvim-dap
-return {
-  "rcarriga/nvim-dap-ui",
-  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-}
--- dotfiles/neovim/lua/plugins/nvim-dap.lua
+++
@@ -1,8 +0,0 @@
--- nvim-dap is a debug adapter protocol for neovim
-return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "mfussenegger/nvim-dap-python",
-    "leoluz/nvim-dap-go",
-  },
-}
