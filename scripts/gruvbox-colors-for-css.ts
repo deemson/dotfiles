baf54ac6368fd93c37e40b0a17a7811b7b085668
deemson <deemson.k@gmail.com>
2025-05-12 07:10:33 +0200

    added color scripts

---
+++ scripts/gruvbox-colors-for-css.ts
@@ -0,0 +1,24 @@
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+
+const dotfilesDir = path.dirname(path.resolve(import.meta.dirname));
+const filePath = path.join(dotfilesDir, "colors", "morhetz-gruvbox", "gruvbox-transformed.json");
+
+type Cell = [string, string];
+type Row = Cell[];
+
+(async () => {
+  const fileData = await fs.readFile(filePath);
+  const gruvboxJSON = JSON.parse(fileData.toString()).dark;
+  console.log(
+    gruvboxJSON
+      .map((row: Row) => {
+        return row
+          .map(([name, value]) => {
+            return `@define-color ${name} #${value};`;
+          })
+          .join("\n");
+      })
+      .join("\n\n"),
+  );
+})();
---
+++ scripts/gruvbox-colors-transform-raw-md-json.ts
@@ -0,0 +1,41 @@
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+
+const dotfilesDir = path.dirname(path.resolve(import.meta.dirname));
+const colorsDir = path.join(dotfilesDir, "colors", "morhetz-gruvbox");
+const inPath = path.join(colorsDir, "gruvbox-for-md.json");
+const outPath = path.join(colorsDir, "gruvbox-transformed.json");
+
+type Cell = [string, string];
+type Row = Cell[];
+
+const transformColorArrays = (rows: Row[]): Row[] => {
+  return rows.map((row: Row, iRow: number) => {
+    return row
+      .filter(([name, _]) => {
+        const isNamePresent = !!name;
+        const isSecondGray = iRow === 2 && name === "gray";
+        return isNamePresent && !isSecondGray;
+      })
+      .map(([name, value]) => {
+        let correctedName = name;
+        if (iRow < 2 && !(name === "fg" || name === "bg")) {
+          correctedName += iRow + 1;
+        }
+        if (name === "orange") {
+          correctedName += iRow - 1;
+        }
+        return [correctedName, value];
+      });
+  });
+};
+
+(async () => {
+  const fileData = await fs.readFile(inPath);
+  const { dark, light } = JSON.parse(fileData.toString());
+  const transformed = {
+    dark: transformColorArrays(dark),
+    light: transformColorArrays(light),
+  };
+  await fs.writeFile(outPath, JSON.stringify(transformed));
+})();
