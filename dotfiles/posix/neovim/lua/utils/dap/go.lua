cb5747beccf7e06b0ddb295946e28aee8abbc9e3
deemson <deemson.k@gmail.com>
2026-03-31 10:34:30 +0200

    neovim: improvements for go dap utils

--- dotfiles/posix/neovim/lua/utils/dap/go.lua
+++ dotfiles/posix/neovim/lua/utils/dap/go.lua
@@ -1,6 +1,29 @@
 local dap = require("dap")
 local M = {}
 
+M.add_local = function()
+  if dap.adapters.go_local ~= nil then
+    vim.notify("Go local adapter already exists", vim.log.levels.ERROR)
+    return
+  end
+  dap.adapters.go_local = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = "dlv",
+      args = { "dap", "--listen", "127.0.0.1:${port}" },
+    },
+  }
+  dap.configurations.go = vim.list_extend(dap.configurations.go or {}, {
+    {
+      type = "go_local",
+      name = "Debug file",
+      request = "launch",
+      program = "${file}",
+    },
+  })
+end
+
 ---@class RemoteConfig
 ---@field key string
 ---@field name string
@@ -9,13 +32,16 @@ local M = {}
 
 --- @param config RemoteConfig
 M.add_remote = function(config)
+  if dap.adapters[config.key] ~= nil then
+    vim.notify("Go debug adapter with key '" .. config.key .. "' already exists", vim.log.levels.ERROR)
+    return
+  end
   dap.adapters[config.key] = {
     type = "server",
     host = "127.0.0.1",
     port = config.port,
   }
-  local configs_table = dap.configurations.go or {}
-  vim.list_extend(configs_table, {
+  dap.configurations.go = vim.list_extend(dap.configurations.go or {}, {
     {
       type = config.key,
       name = config.name,
@@ -31,7 +57,6 @@ M.add_remote = function(config)
       },
     },
   })
-  dap.configurations.go = configs_table
 end
 
 return M
