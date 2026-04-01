75f083c6ae130aeecf61495105d76a6b86b89150
deemson <deemson.k@gmail.com>
2026-04-02 01:15:44 +0200

    neovim: lua -- better neovim settings detection

--- dotfiles/posix/neovim/lsp/lua_language_server.lua
+++ dotfiles/posix/neovim/lsp/lua_language_server.lua
@@ -17,9 +17,12 @@ return {
   },
   on_attach = function(client, bufnr)
     local buf_file_path = vim.api.nvim_buf_get_name(bufnr)
+    local is_inside_vim_runtime = vim.startswith(buf_file_path, vim.env.VIMRUNTIME)
     local is_inside_config = vim.startswith(buf_file_path, vim.fn.stdpath("config"))
+    local is_inside_data = vim.startswith(buf_file_path, vim.fn.stdpath("data"))
+    local is_nvim_dir = is_inside_vim_runtime or is_inside_config or is_inside_data
     local is_dot_nvim_lua = vim.endswith(buf_file_path, ".nvim.lua")
-    if is_inside_config or is_dot_nvim_lua then
+    if is_nvim_dir or is_dot_nvim_lua then
       client.settings = {
         Lua = {
           runtime = { version = "LuaJIT" },
