5304f6a73cefb18e2df9ea5af851bd3ec1dae8ac
deemson <deemson.k@gmail.com>
2026-04-16 00:19:04 +0200

    hammerspoon: half-split hotkeys

--- dotfiles/macos/hammerspoon/init.lua
+++ dotfiles/macos/hammerspoon/init.lua
@@ -12,6 +12,28 @@ hs.hotkey.bind({ "cmd", "ctrl" }, "return", function()
   hs.window.focusedWindow():maximize()
 end)
 
+hs.hotkey.bind({ "cmd", "ctrl" }, "left", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen():frame()
+  win:setFrame({
+    x = screen.x,
+    y = screen.y,
+    w = screen.w / 2,
+    h = screen.h,
+  })
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "right", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen():frame()
+  win:setFrame({
+    x = screen.x + screen.w / 2,
+    y = screen.y,
+    w = screen.w / 2,
+    h = screen.h,
+  })
+end)
+
 local changeWindowStep = 100
 
 hs.hotkey.bind({ "cmd", "ctrl" }, "[", function()
@@ -25,4 +47,3 @@ end)
 hs.hotkey.bind({ "cmd", "ctrl" }, "k", utils.makeAppLauncher("KeePassXC"))
 hs.hotkey.bind({ "cmd", "ctrl" }, "o", utils.makeAppLauncher("Obsidian"))
 hs.hotkey.bind({ "cmd", "ctrl" }, "b", utils.makeAppLauncher("Google Chrome"))
-
