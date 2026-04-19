78490ad397cf2c364c45ba244f36049098eb03e1
deemson <deemson.k@gmail.com>
2026-04-19 20:11:59 +0200

    neovim: proper LSP restart command

--- dotfiles/posix/neovim/lua/config/lsp.lua
+++ dotfiles/posix/neovim/lua/config/lsp.lua
@@ -22,8 +22,13 @@ vim.lsp.enable({
 })
 
 vim.api.nvim_create_user_command("LspRestart", function()
-  vim.lsp.stop_client(vim.lsp.get_clients({ bufnr = 0 }))
-  vim.cmd("e")
+  local lsp_clients = vim.lsp.get_clients({ bufnr = 0 })
+  local lsp_client_names = vim.tbl_map(function(lsp_config)
+    return lsp_config.name
+  end, lsp_clients)
+
+  vim.lsp.stop_client(lsp_clients)
+  vim.lsp.enable(lsp_client_names)
 end, { desc = "Restart LSP" })
 
 vim.api.nvim_create_user_command("LspLog", function()
