c7f3a713069fac04d6f606ad7f28ccb6f00dff16
deemson <deemson.k@gmail.com>
2026-05-19 15:12:36 +0200

    neovim: cleanup LSP config

--- dotfiles/posix/neovim/lua/config/lsp.lua
+++ dotfiles/posix/neovim/lua/config/lsp.lua
@@ -26,15 +26,13 @@ vim.lsp.enable({
 vim.api.nvim_create_user_command("LspRestart", function()
   local lsp_clients = vim.lsp.get_clients({ bufnr = 0 })
   local lsp_client_names = vim.tbl_map(function(lsp_config)
+    lsp_config:stop()
     return lsp_config.name
   end, lsp_clients)
 
-  vim.lsp.stop_client(lsp_clients)
   vim.lsp.enable(lsp_client_names)
 end, { desc = "Restart LSP" })
 
 vim.api.nvim_create_user_command("LspLog", function()
-  vim.cmd("edit " .. vim.lsp.get_log_path())
+  vim.cmd("edit " .. vim.lsp.log.get_filename())
 end, { desc = "LSP Logs" })
-
-vim.lsp.set_log_level("debug")
