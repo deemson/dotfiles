ffbc9b7f7b56d054fbb88f7e841fd536c54fe600
deemson <deemson.k@gmail.com>
2026-05-20 03:28:17 +0200

    neovim: treesitter folds

--- dotfiles/posix/neovim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/posix/neovim/lua/plugins/nvim-treesitter.lua
@@ -7,23 +7,32 @@ return {
     local treesitter = require("nvim-treesitter")
     local treesitter_config = require("nvim-treesitter.config")
 
+    local function start(buf, lang)
+      vim.treesitter.start(buf, lang)
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo[0][0].foldmethod = "expr"
+    end
+
     vim.api.nvim_create_autocmd("FileType", {
       callback = function(args)
+        vim.o.foldlevel = 99
+        vim.o.foldenable = false
+
         local ft = args.match
         local lang = vim.treesitter.language.get_lang(ft) or ft
 
         -- already installed? just start highlighting
         if vim.list_contains(treesitter_config.get_installed(), lang) then
-          vim.treesitter.start(args.buf, lang)
+          start(args.buf, lang)
           return
         end
 
         -- not installed, but available -> install then start
         if vim.list_contains(treesitter.get_available(), lang) then
-          vim.notify("installing treesitter parser '" .. lang .."' for " .. ft, vim.log.levels.INFO)
+          vim.notify("installing treesitter parser '" .. lang .. "' for " .. ft, vim.log.levels.INFO)
           treesitter.install(lang):await(function()
             if vim.api.nvim_buf_is_valid(args.buf) then
-              vim.treesitter.start(args.buf, lang)
+              start(args.buf, lang)
             end
           end)
         end
