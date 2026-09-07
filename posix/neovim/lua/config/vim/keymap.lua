5f3ffe9660709e70d8eeb83fa0ff6ff9c2fefac3
deemson <deemson.k@gmail.com>
2026-09-07 17:36:21 +0200

    neovim: reference keybind

--- posix/neovim/lua/config/vim/keymap.lua
+++ posix/neovim/lua/config/vim/keymap.lua
@@ -9,6 +9,31 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank into system bu
 -- same as above but for the entire line and only in normal mode
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line into system buffer" })
 
+local function yank_selection_reference()
+  local path = vim.api.nvim_buf_get_name(0)
+  if path == "" then
+    vim.notify("Cannot reference an unnamed buffer", vim.log.levels.WARN)
+    return
+  end
+
+  local first_line = vim.fn.line("v")
+  local last_line = vim.fn.line(".")
+  if first_line > last_line then
+    first_line, last_line = last_line, first_line
+  end
+
+  local relative_path = vim.fn.fnamemodify(path, ":.")
+  local line_range = first_line == last_line and tostring(first_line)
+    or string.format("%d-%d", first_line, last_line)
+  local reference = string.format("%s:%s", relative_path, line_range)
+
+  vim.fn.setreg("+", reference)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
+  vim.notify("Yanked reference: " .. reference)
+end
+
+vim.keymap.set("x", "<leader>yr", yank_selection_reference, { desc = "Yank selection reference" })
+
 vim.keymap.set("i", "<C-,>", "<C-d>", { noremap = true })
 vim.keymap.set("i", "<C-.>", "<C-t>", { noremap = true })
 
